a5e77b08682449604632219b7041ebe7a0f32ce4
Felippe Chemello <felippechemello@gmail.com>
2020-12-29 00:08:24 -0300

    Adicionando links na index

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -2,9 +2,11 @@ import Link from 'next/link'
 
 export default function Index() {
     return (
-        <main>
+        <main style={{ display: 'flex', flexDirection: 'column' }}>
             <h1> Hello World </h1>
             <Link href='/signin'>Login</Link>
+            <Link href='/signup'>Cadastro</Link>
+            <Link href='/dashboard'>Dashboard</Link>
         </main>
     )
 }
